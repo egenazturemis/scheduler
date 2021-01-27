883736abb6cdeec773b820c12455c16af216f9d8
Egenaz Turemis <egenazturemis@gmail.com>
2021-01-27 18:40:10 -0500

    make all getInterviewersForDay tests pass

--- src/helpers/selectors.test.js
+++ src/helpers/selectors.test.js
@@ -6,13 +6,13 @@ const state = {
       id: 1,
       name: "Monday",
       appointments: [1, 2, 3], 
-      interviewers: []
+      interviewers: [1]
     },
     {
       id: 2,
       name: "Tuesday",
       appointments: [4, 5],
-      interviewers: []
+      interviewers: [2, 1]
     }
   ],
   appointments: {
@@ -98,13 +98,13 @@ test("getInterviewersForDay returns an array", () => {
 
 test("getInterviewersForDay returns an array with a length matching the number of appointments for that day", () => {
   const result = getInterviewersForDay(state, "Monday");
-  expect(result.length).toEqual(3);
+  expect(result.length).toEqual(1);
 });
 
 test("getInterviewersForDay returns an array containing the correct appointment objects", () => {
   const [first, second] = getInterviewersForDay(state, "Tuesday");
-  expect(first).toEqual(state.appointments["4"]);
-  expect(second).toEqual(state.appointments["5"]);
+  expect(first).toEqual(state.interviewers["2"]);
+  expect(second).toEqual(state.interviewers["1"]);
 });
 
 test("getInterviewersForDay returns an empty array when the days data is empty", () => {
